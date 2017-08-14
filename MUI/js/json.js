c61075c4c88879c3f3abbe2193c6cf6fb773f8dd
zhanglei1994 <577725356@qq.com>
2017-08-14 11:17:33 +0800

    MUI
    bilibili

---
+++ MUI/js/json.js
@@ -0,0 +1,66 @@
+var jsonData = {
+	"reason":"查询成功",
+	"result":{
+		"title":"黑客帝国",
+		"tag":"动作 / 恐怖 / 科幻",
+		"act":"基努·李维斯 凯瑞·安·莫斯 劳伦斯·菲什伯恩 基努·里维斯 凯瑞-安·莫斯 雨果·维文 格洛丽亚·福斯特 乔·潘托里亚诺",
+		"year":"1999",
+		"rating":8.8,
+		"area":"欧美",
+		"dir":"安迪·沃卓斯基 / 拉里·沃卓斯基 / 拉娜·沃卓斯基 / Lana Wachowski",
+		"desc":"基努李维斯又带来了一部令人兴奋的电影。本片被许多影评人评为1999年最棒的科幻影片。 在不久的将来，一名年轻的网络黑客尼奥基努李维斯发现看似正常的现实世界实际上是由一个名为“矩阵”的计算机人工智能系统控制的，人们就象他放牧的动物一样。尼奥在网络上查找关于“矩阵”的一切。在。在一名神秘女郎特蕾妮嘉丽安莫斯的引导下见到了黑客组织的首领摩菲斯劳伦斯费许本，三人一起走上了抗争“矩阵”的征途。 “尼奥，你曾经作过这样的梦吗，你坚信不移的东西都是真的吗？你能从那样的梦中醒来吗？你能分辨出梦境与现实世界的区别吗？”尼奥不顾一切地想追寻“矩阵”的真相，他在梦中总是听到关于它的耳语，这是一种神秘的不可知的东西，他清楚地感觉到自己正在受它的控制。 “矩阵是什么？”尼奥不知道，但是他相信摩菲斯这个在传说中被描绘成非常危险人物的黑客能够给他答案。 摩菲斯：“没有人能够说明矩阵是什么，你只有亲自去看。” 尼奥面对着另外一个问题：他就是它吗？虽然不知道为什么，尼奥的答案就是“不是”。摩菲斯的朋友塞弗同意他的观点。而其他人则不那么肯定。 另外，还有人在保护“矩阵”，冷酷的史密斯探员一伙掌握着令人震惊的秘密。 尼奥、默菲和特莱妮必须用他们的生命对抗强大的敌人来追寻答案、寻找自我。 他们的每一步都是危机四伏",
+		"cover":"http://p2.qhimg.com/t01381f5f0577d6623e.jpg",
+		"vdo_status":"none",
+		"playlinks":{
+			"sohu":"",
+			"qq":"",
+			"imgo":"",
+			"youku":""
+		},
+		"video_rec":[
+			{
+				"cover":"http://p2.qhimg.com/d/dy_0c653ae1845ba1f0e41c7459ca91bd1e.jpg",
+				"detail_url":"http://www.360kan.com/m/g6nlZBH3SHT7UR.html",
+				"title":"黑客帝国3"
+			},
+			{
+				"cover":"http://p4.qhimg.com/t01a03dbd37f4d256ca.jpg",
+				"detail_url":"http://www.360kan.com/m/gqvpY0P8QXb0Sh.html",
+				"title":"黑客帝国2：重装上阵"
+			},
+			{
+				"cover":"http://p0.qhimg.com/t019a9da2d65b45f46b.jpg",
+				"detail_url":"http://www.360kan.com/m/f6fkYkP8QXb0SR.html",
+				"title":"黑客帝国3：矩阵革命"
+			},
+			{
+				"cover":"http://p7.qhimg.com/d/dy_bc35854e1e5dca18bf68f5347ee009af.jpg",
+				"detail_url":"http://www.360kan.com/m/gabnYhH2RXH4UB.html",
+				"title":"黑客帝国：矩阵革命"
+			},
+			{
+				"cover":"http://p1.qhimg.com/d/dy_8d02d9e235cc91cb572725c52a8a2a4c.jpg",
+				"detail_url":"http://www.360kan.com/m/fqLqZETBQ0n0SB.html",
+				"title":"网络杀人事件"
+			}
+		],
+		"act_s":[
+			{
+				"name":"基努·李维斯",
+				"url":null,
+				"image":"http://p2.qhmsg.com/dmsmty/120_110_100/t010bde3034da69759d.jpg"
+			},
+			{
+				"name":"凯瑞·安·莫斯",
+				"url":null,
+				"image":null
+			},
+			{
+				"name":"劳伦斯·菲什伯恩",
+				"url":null,
+				"image":"http://p8.qhmsg.com/dmsmty/120_110_100/t01d60d34109959dd63.jpg"
+			}
+		]
+	},
+	"error_code":0
+}
---
+++ MUI/js/myQuery.js
@@ -0,0 +1,74 @@
+function $(id){ //获取元素样式封装
+			return document.getElementById(id);
+		}
+function getStyle(obj,attr){ // 获取元素属性封装
+			if(obj.currentStyle){
+				return (obj.currentStyle[attr]);
+			}else{
+				return (getComputedStyle(obj)[attr]);
+			}
+		}
+function Go(obj, attr, direct, target, endFn) { //对象 属性 方向 目标 函数封装
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function() {
+		//为什么要写成obj.timer,为了解决多个元素的动画相互之间影响的问题
+		var Distance = parseInt(getStyle(obj, attr)) + direct;
+		if(Distance >= target && direct > 0 || Distance < target && direct < 0) { //与运算的优先级大于或运算
+			Distance = target;
+		}
+		//			1+2*3 &&运算符的优先级大于||
+		obj.style[attr] = Distance + 'px';
+		//				obj.style.marginLeft
+		if(Distance == target) {
+			clearInterval(obj.timer);
+			if(endFn) { //这个if判断是为了解决我们不需要传入endFn参数时的报错问题
+				endFn(); //实际调用Go函数的时候，我们有可能需要再Go函数执行完毕之后去做其他事情，这件事就通过回调函数endFn来实现
+			}
+		}
+
+	}, 40)
+}
+function fn(obj,math,endFn){ //透明度函数封装
+			obj.changeOp =setInterval(function(){
+			var op=parseFloat(getStyle(obj,'opacity'))+math;
+				if(op>1 || op<0){
+//					console.log(op)
+					
+					math<0? op=0 : op=1;
+					if(op<=1){
+						obj.style.display='none';
+					}
+					clearInterval(obj.changeOp)
+					if(endFn){
+							endFn()
+						}
+				}
+//				console.log(op)
+				obj.style.opacity=op;
+//				console.log(op)
+			},50)
+			
+}
+function shake(obj,attr,endFn){ //图片抖动函数封装
+//			if(obj.s){
+//				obj.s=false;
+					var Left=parseInt(getStyle(obj,attr));
+	//			var arr=[20,-20,18,-18,16,-16,14,-14,12,-12,10,-10,8,-8,6,-6,4,-4,2,-2,0];
+				var arr=[];
+				for(var i=20;i>=0;i-=2){
+					arr.push(i,-i)
+				}
+				var num=0;
+				obj.timer = setInterval(function(){
+					obj.style[attr]=Left+arr[num]+'px';
+					num++;
+					if(num>=arr.length){
+						clearInterval(obj.timer);
+						obj.s=true;
+						if(endFn){
+							endFn()
+						}
+					}
+				},40)
+//			}
+}
